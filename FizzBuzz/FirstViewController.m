4dc61d4c1a64b4bb470020f68d3ac16b88b2a799
Bhanu Birani <bhanubirani@gmail.com>
2015-07-08 15:30:22 +0530

    Fizbuzz commit

--- FizzBuzz/FirstViewController.m
+++ FizzBuzz/FirstViewController.m
@@ -8,7 +8,14 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+#define WIDTH 50
+#define HEIGHT 50
+
+@interface FirstViewController () {
+    UITapGestureRecognizer *tapGesture;
+    NSMutableArray *viewArray;
+    int viewTag;
+}
 
 @end
 
@@ -16,12 +23,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHandler:)];
+    [self.view addGestureRecognizer:tapGesture];
+    
+    viewTag = 1;
+    viewArray = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)createOrReplaceViewAtLocaion:(CGPoint)location {
+    
+    if (viewArray.count >= 52) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert!" message:@"Can not add any furhter boxes" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+    
+    BOOL didViewExistAtLocation = false;
+    UIView *overlappingView;
+    CGSize deviceSize = [UIScreen mainScreen].bounds.size;
+    for (UIView *square in viewArray) {
+        if (CGRectContainsPoint(square.frame, location)) {
+            // Move the square next to the existing from right side of screen
+            didViewExistAtLocation = YES;
+            overlappingView = square;
+            break;
+        }
+    }
+    if (didViewExistAtLocation) {
+        NSInteger row = (overlappingView.tag-1)*WIDTH+(overlappingView.tag-1)*10+10;
+        NSInteger col = HEIGHT;
+        if (row+WIDTH > deviceSize.width) {
+            NSInteger factorCol = (viewArray.count/6 + 1);
+            col = HEIGHT*factorCol + 10 *factorCol;
+            NSInteger factorRow = (overlappingView.tag-1)%6;
+            row = factorRow*WIDTH+factorRow*10+10;
+        }
+        [overlappingView setFrame:CGRectMake(row, col, WIDTH, HEIGHT)];
+    }
+    UIView *square = [[UIView alloc] initWithFrame:CGRectMake(location.x-WIDTH/2, location.y-HEIGHT/2, WIDTH, HEIGHT)];
+    square.tag = viewTag;
+    [square setBackgroundColor:[UIColor lightGrayColor]];
+    [viewArray addObject:square];
+    [self.view addSubview:square];
+    viewTag++;
+}
+
+- (void)tapHandler:(UIGestureRecognizer *)sender {
+    CGPoint targetPoint = [sender locationInView:self.view];
+    [self createOrReplaceViewAtLocaion:targetPoint];
+}
+
 
 @end
--- FizzBuzz/SecondViewController.m
+++ FizzBuzz/SecondViewController.m
@@ -8,7 +8,9 @@
 
 #import "SecondViewController.h"
 
-@interface SecondViewController ()
+@interface SecondViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 
 @end
 
@@ -23,5 +25,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma TableView Delegate/Datasource methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1000;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"data_cell" forIndexPath:indexPath];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"data_cell"];
+    }
+    
+    NSInteger cellIndex = indexPath.row + 1;
+    NSString *titleString;
+    if ((cellIndex % 3 == 0) && (cellIndex % 5 == 0)) {
+        titleString = @"Fizz Buzz";
+    } else if (cellIndex % 3 == 0) {
+        titleString = @"Fizz";
+    } else if (cellIndex % 5 == 0) {
+        titleString = @"Buzz";
+    } else {
+        titleString = [NSString stringWithFormat:@"%ld", (long)cellIndex];
+    }
+    
+    cell.textLabel.text = titleString;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 
 @end
